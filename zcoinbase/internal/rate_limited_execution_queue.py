5200637dedeb3efee887d35a0d21cc9ac7903e00
Chris <chrisfwin@gmail.com>
2021-01-04 20:18:03 -0500

    Improve RateLimitedExecutionQueue with type hints.

--- zcoinbase/internal/rate_limited_execution_queue.py
+++ zcoinbase/internal/rate_limited_execution_queue.py
@@ -2,11 +2,11 @@ from contextlib import AbstractContextManager
 from functools import partial
 from multiprocessing.pool import ThreadPool
 from threading import BoundedSemaphore, Thread, Event
-from typing import Callable
+from typing import Callable, Any
 
 
 class RateLimitedExecutionQueue(AbstractContextManager):
-  def __init__(self, max_calls_per_interval: int, interval: int = 1):
+  def __init__(self, max_calls_per_interval: int, interval: float = 1):
     self.max_calls_per_interval = max_calls_per_interval
     self.interval = interval
     self._call_limiter = BoundedSemaphore(self.max_calls_per_interval)
@@ -27,11 +27,11 @@ class RateLimitedExecutionQueue(AbstractContextManager):
     self.close_and_join()
 
   @staticmethod
-  def _function_wrapper(limiter: BoundedSemaphore, function: Callable):
+  def _function_wrapper(limiter: BoundedSemaphore, function: Callable[[], None]):
     limiter.acquire()
     return function()
 
-  def add_function_to_pool(self, function: Callable):
+  def add_function_to_pool(self, function: Callable[[], Any]):
     return self._execution_pool.apply_async(
       partial(RateLimitedExecutionQueue._function_wrapper, self._call_limiter, function))
 
